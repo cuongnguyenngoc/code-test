9f1ef2a8e1586a8f6bf32da0fdea46c2eef5260d
justin <justinnguyen@tatsu.works>
2025-12-11 09:10:39 +0700

    add 3 ways of sum to n

---
+++ problem1/sum-to-n.js
@@ -0,0 +1,59 @@
+// adjust final result based on original sign of n
+// if n was negative, return negative sum, otherwise return positive sum.
+const applySign = (n, sum) => (n < 0 ? -sum : sum);
+
+// 3 functions bellow use Math.abs and applySign to support negative n
+// Note: Assuming this input n will always produce a result lesser than Number.MAX_SAFE_INTEGER as the requirement stated.
+const sum_to_n_a = function(n) {
+    // use positive number
+    let absN = Math.abs(n);
+    let sum = 0;
+    // sum 1 + 2 + ... + absN
+    for (let i = 1; i <= absN; i++) {
+        sum += i;
+    }
+
+    // reapply original sign of n for correct sign of sum
+    return applySign(n, sum);
+};
+
+const sum_to_n_b = function(n) {
+    // use positive number
+    let absN = Math.abs(n);
+    let sum = 0;
+    let current = absN;
+
+    // sum absN + (absN-1) + ... + 1
+    while (current >= 1) {
+        sum += current;
+        current--;
+    }
+
+    // reapply original sign of n for correct sum
+    return applySign(n, sum);
+};
+
+const sum_to_n_c = function(n) {
+    // use positive number
+    let absN = Math.abs(n);
+
+    // Base case: sum to 1 is 1
+    if (absN == 1) return 1;
+    
+    // Recursive case: n + sum(n-1)
+    const sum = absN + sum_to_n_c(absN-1);
+
+    // reapply original sign of n for correct sum
+    return applySign(n, sum);
+};
+
+function main() {
+    const sumA = sum_to_n_a(5);
+    const sumB = sum_to_n_b(5);
+    const sumC = sum_to_n_c(5);
+    console.log("sumA", sumA);
+    console.log("sumB", sumB);
+    console.log("sumC", sumC);
+}
+
+main();
