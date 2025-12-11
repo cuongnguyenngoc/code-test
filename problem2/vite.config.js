7c15a8253dbec723e53468a035211d34c58d1e8a
justin <justinnguyen@tatsu.works>
2025-12-11 14:40:41 +0700

    correct outputDir

--- problem2/vite.config.js
+++ problem2/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   root: '.',
   build: {
-    outDir: '../dist',   // output folder (dist at project root)
+    outDir: 'dist',   // output folder (dist at project root)
     emptyOutDir: true
   }
 });
